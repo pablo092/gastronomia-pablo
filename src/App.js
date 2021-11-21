84fc8c3e48cce0e4db20849a7d1abaab9d6198b3
pablo092 <elpabli09@gmail.com>
2021-11-21 00:33:19 -0300

    Desafío: Detalle de Producto

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import NavBar from "./components/NavBar";
+import ItemDetailContainer from "./container/ItemDetailContainer";
 import ItemListContainer from "./container/ItemListContainer";
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <div className="App">
       <NavBar />
       <ItemListContainer />
+      <ItemDetailContainer />
     </div>
   );
 }
---
+++ src/container/ItemDetailContainer.jsx
@@ -0,0 +1,51 @@
+import { useState, useEffect } from "react";
+import ItemDetail from "../components/ItemDetail";
+
+/* Esta función debe retornar la promesa que resuelva con delay */
+const getItems = new Promise((res, rej) => {
+  setTimeout(() => {
+    res([
+      {
+        id: 1,
+        title: "Item1",
+        pictureUrl: "http://via.placeholder.com/450",
+        description:
+          "es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen.",
+        price: "$500",
+      },
+      {
+        id: 2,
+        title: "Item2",
+        pictureUrl: "http://via.placeholder.com/450",
+        description:
+          'No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicos, quedando esencialmente igual al original. Fue popularizado en los 60s con la creación de las hojas "Letraset".',
+        price: "$1000",
+      },
+    ]);
+  }, 2000);
+});
+
+// Al iniciar utilizando un efecto de montaje, debe llamar a un async mock (promise)
+// que en 2 segundos le devuelva un 1 ítem, y lo guarde en un estado propio.
+
+function ItemDetailContainer() {
+  // Implementar mock invocando a getItems() y utilizando el resolver then
+  const [itemDetailResq, setItemDetailResq] = useState();
+
+  useEffect(() => {
+    getItems.then((res) => {
+      setItemDetailResq(res[0]);
+    });
+  }, []);
+  return (
+    <>
+      {itemDetailResq === undefined ? (
+        <p>loading</p>
+      ) : (
+        <ItemDetail item={itemDetailResq}/>
+      )}
+    </>
+  );
+}
+
+export default ItemDetailContainer;
