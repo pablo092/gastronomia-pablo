af2cb2d2d56c9ba91fca1c68d9ff933915cfa40f
pablo092 <elpabli09@gmail.com>
2021-11-30 23:57:41 -0300

    Desafío: Sincronizar counter

---
+++ src/components/ItemCount.js
@@ -0,0 +1,42 @@
+import { useState } from "react";
+import { useCartContext } from "../context/CartContext";
+
+export default function ItemCount(item) {
+  const [count, setCount] = useState(0);
+  const { cartList, addItem } = useCartContext();
+
+  console.log(cartList);
+
+  const sumarContador = () => {
+    if (count < 5) {
+      setCount(count + 1);
+    } else {
+      alert("La cantidad supera el stock disponible");
+    }
+  };
+
+  const restarContador = () => {
+    if (count > 1) {
+      setCount(count - 1);
+    } else {
+      alert("¿Eliminar item del carrito?");
+      setCount(0);
+    }
+  };
+
+  const agregarCarrito = () => {
+    setCount(count);
+    addItem({item}, count);
+  }
+
+  return (
+    <div>
+      <button onClick={sumarContador}>+</button>
+      <p>{count}</p>
+      <button onClick={restarContador}>-</button>
+      <div>
+        <button onClick={() => agregarCarrito}>Agregar Carrito</button>
+      </div>
+    </div>
+  );
+}
--- src/components/ItemDetail.js
+++ src/components/ItemDetail.js
@@ -1,20 +1,49 @@
-import {useState } from 'react'
-import { Col, Row } from 'react-bootstrap'
-import { useCartContext } from '../context/CartContext'
+import Button from "@restart/ui/esm/Button";
+import { useState } from "react";
+import { Col, Row } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import ItemCount from "./ItemCount";
 
 // ItemDetail.js, que debe mostrar la vista de detalle de un ítem incluyendo su descripción, una foto y el precio.
-export const ItemDetail = ({ item }) => {
+const ItemDetail = ({ item }) => {
+  const [addPressed, setAddPressed] = useState(false);
+
+  function onAdd() {
+    setAddPressed(true);
+  }
+
+  // Desarrolla la vista de detalle expandida del producto con su imagen título, descripción y precio
   return (
-    <>
-      {/* Desarrolla la vista de detalle expandida del producto con su imagen título, descripción y precio */}
-      <div>
-        <p>{item.title}</p>
-        <img src={item.pictureUrl} alt="pictureUrl" />
-        <p>{item.description}</p>
-        <p>{item.price}</p>
-      </div>
-    </>
+    <Row>
+      <Col>
+        <div className="card w-50">
+          <div className="container">
+            <label>{item.title}</label>
+          </div>
+          <div className="container">
+            <img src={item.pictureUrl} className="w-25" alt="foto" />
+            <br />
+            <label>{item.descripcion}</label>
+          </div>
+          <div className="container">
+            <label>{item.price}</label>
+          </div>
+        </div>
+      </Col>
+      <Col>
+        {/* Oculto hasta que el usuario seleccione la cantidad y clickee en comprar */}
+        {!addPressed ? (
+          <div>
+            <ItemCount onClick={onAdd()} item={item} />
+          </div>
+        ) : (
+          <Link to={`/cart`}>
+            <Button>Termina tu compra</Button>
+          </Link>
+        )}
+      </Col>
+    </Row>
   );
-}
+};
 
 export default ItemDetail;
